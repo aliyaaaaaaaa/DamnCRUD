916a83168ab71bb0846862cb4544d367f33a8ac0
anyaaa <124210105+aliyaaaaaaaa@users.noreply.github.com>
2025-03-18 09:23:48 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # entrypoint.sh
+set -e
+
+# Cek readiness MySQL
+echo "Menunggu database siap..."
+until mysqladmin ping -h db -u root -proot --silent; do
+    echo "Database belum siap. Menunggu..."
+    sleep 2
+done
+echo "Database sudah siap!"
 
 # Jalankan PHP server di background
 php -S 0.0.0.0:8000 -t /var/www/html &
